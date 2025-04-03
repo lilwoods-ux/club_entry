62d02d310a3b801f002cc2afd8dc3c678efd0db9
lilwoods-ux <mdogotimothy05@gmail.com>
2025-04-03 21:43:18 +0300

    second commit

--- src/clubentry.kt
+++ src/clubentry.kt
@@ -5,26 +5,35 @@ fun main() {
     val name = readln()
     print("enter your age:")
     val age = readln().toIntOrNull() ?: 0
-    print("Do You Have A VIP Pass? (yes/no)")
-    val hasVipPass = readln().equals("yes", ignoreCase = true)
-    print("Are you a member? (yes/no)")
-    val ismember = readln().equals("yes", ignoreCase = true)
-    if (age <= 18) {
-        println("NO ENTRY!!")
-    } else {
-        println("WELCOME TO OUR CLUB!!")
 
-        if (hasVipPass) {
-            println("WELCOME OUR VIP")
+    when {
+        age <= 18 -> {
+            println("NO ENTRY!!")
 
-        } else {
-            print("please enter your members number:")
-            val membersnumber = readln().toIntOrNull() ?: 0
-            println("Drink discount,member $membersnumber")
         }
-        println(name)
-        println(age)
-        println(hasVipPass)
-        println(ismember)
+
+        else -> {
+            print("Do You Have A VIP Pass? (yes/no)")
+            val hasVipPass = readln().equals("yes", ignoreCase = true)
+            println("WELCOME TO OUR CLUB!!")
+
+            when {
+                hasVipPass -> {
+                    println("WELCOME OUR VIP")
+
+                }
+
+                else -> {
+
+                    print("please enter your members number:")
+                    val membersnumber = readln().toIntOrNull() ?: 0
+                    println("Drink discount,member $membersnumber")
+                }
+
+
+            }
+        }
     }
+    println(name)
+    println (age)
 }
